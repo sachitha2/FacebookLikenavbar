e8f08e83dfa909304c3757df42bb199180d0f098
Sachitha Hirushan <shpsachitha@gmail.com>
2021-05-22 13:18:42 +0530

    now css transissions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
+import {ReactComponent as ArrowIcon} from './icons/arrow.svg';
+import {ReactComponent as BellIcon} from './icons/bell.svg';
+import {ReactComponent as BoltIcon} from './icons/bolt.svg';
+import {ReactComponent as ChevronIcon} from './icons/chevron.svg';
+import {ReactComponent as CogIcon} from './icons/cog.svg';
+import {ReactComponent as MessengerIcon} from './icons/messenger.svg';
+import {ReactComponent as PlusIcon} from './icons/plus.svg';
+import {ReactComponent as CaretIcon} from './icons/caret.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <NavBar>
+    <NavItem icon={<PlusIcon/>}/>
+    <NavItem icon={<BellIcon/>}/>
+    <NavItem icon={<MessengerIcon/>}/>
+
+    <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+    </NavItem>
+   </NavBar>
   );
+}
+
+function NavBar(props){
+  return(
+    <nav className="navBar">
+      <ul className="navBar-nav">
+        {props.children}
+      </ul>
+    </nav>
+  )
+}
+
+
+function NavItem(props){
+  const [open,setOpen] = useState(false);
+return (
+  <li className="nav-item">
+    <a href="#" className="icon-button" onClick={()=>setOpen(!open)}>
+      {props.icon}
+
+
+    </a>
+
+    {open && props.children}
+  </li>
+)
+}
+
+function DropdownMenu(){
+  function DropdownItem(props){
+    return(
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    )
+  }
+  return(
+    <div className="dropdown">
+      <DropdownItem >My Profile</DropdownItem>
+      <DropdownItem leftIcon={<CogIcon/>} rightIcon={<ChevronIcon/>}></DropdownItem>
+    </div>
+  )
 }
 
 export default App;
